1c800cd1914481e339c664f015d2fc3f2ef9010d
souldrafter-dev <souldrafter.dev@gmail.com>
2026-09-08 23:00:58 +0300

    Initial commit: base modem reset logic

---
+++ modem_resseter.py
@@ -0,0 +1,29 @@
+import subprocess
+import time
+from datetime import datetime
+
+# ID модема из диспетчера устройств (win x - Диспетчер устройство - Сетевые Адаптеры)
+DEVICE_ID = r"USB\VID_0BDA&PID_B82C&MI_02\7&9CF2C2F&0&0002"
+# Интервал перезапуска в секундах
+DELAY_SECONDS = 300
+
+print("=== Скрипт автоматического перезапуска USB-модема запущен ===")
+
+while True:
+    current_time = datetime.now().strftime("%H:%M:%S") # Для указания времени на каждую итерацю (перезапуск модема)
+    print(f"[{current_time}] Аппаратный перезапуск USB-модема...")
+    
+    # Запускаем команду pnputil через командную строку Windows
+    try:
+        result = subprocess.run(
+            ["pnputil", "/restart-device", DEVICE_ID], 
+            capture_output=True, 
+            text=True, 
+            check=True
+        )
+        print(f"[{current_time}] Перезапуск завершен успешно.")
+    except subprocess.CalledProcessError as e:     # Вывод ошибки в случае сбоя
+        print(f"[{current_time}] Ошибка при перезапуске: {e.stderr}")
+    
+    print(f"Ожидание {DELAY_SECONDS // 60} минут...\n")
+    time.sleep(DELAY_SECONDS) # Таймер 
